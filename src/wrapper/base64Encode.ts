540426b8a56f4c26cf1c4f12d102e07d1908f53d
dsinghvi <dsinghvi@umich.edu>
2023-12-20 18:12:33 -0500

    update readme

--- src/wrapper/base64Encode.ts
+++ src/wrapper/base64Encode.ts
@@ -1,11 +1,10 @@
 export function base64Encode(str: string): string {
-    if (typeof btoa === "function") {
+    if (typeof Buffer === "function") {
+        // Node.js environment
+        return Buffer.from(str).toString("base64");
+    } else if (typeof btoa === "function") {
         // Browser environment
         return btoa(str);
-    } else if (typeof Buffer === "function") {
-        // Node.js environment
-        const buffer = Buffer.from(str, "utf-8");
-        return buffer.toString("base64");
     } else {
         throw new Error("Base64 encoding not supported in this environment.");
     }
