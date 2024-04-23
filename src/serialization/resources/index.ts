ecd88a8c0c641255810bf9812894586c46233859
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-04-23 14:44:41 +0000

    Release 0.5.0

--- src/serialization/resources/index.ts
+++ src/serialization/resources/index.ts
@@ -1,3 +1,3 @@
-export * as customModels from "./customModels";
 export * as empathicVoice from "./empathicVoice";
+export * as customModels from "./customModels";
 export * as expressionMeasurement from "./expressionMeasurement";
