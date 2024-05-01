695c1afcdfbe56d07c54b948b9bb47cf7fd3c8f6
fern <126544928+fern-bot@users.noreply.github.com>
2024-05-01 17:27:19 -0400

    (fix): send raw audio to socket directly (#23)

--- src/core/websocket/WebSocket.ts
+++ src/core/websocket/WebSocket.ts
@@ -1,14 +1,8 @@
 import * as WebSocket_ from "ws";
-import { RUNTIME } from "../runtime";
 
 export const IsomorphicWebSocket: any = (() => {
-    if (typeof WebSocket !== "undefined") {
-        return WebSocket;
-    }
-    if (typeof global?.WebSocket !== "undefined") {
-        return global.WebSocket;
-    }
-    if (RUNTIME.type === "browser" && typeof window?.WebSocket !== "undefined") {
+    // Use browser Websocket implementation if present
+    if (typeof window != "undefined" && typeof window?.WebSocket !== "undefined") {
         return window.WebSocket;
     }
     if (WebSocket_.WebSocket) { 
