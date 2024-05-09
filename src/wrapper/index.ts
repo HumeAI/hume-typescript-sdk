b0363a982ae274916d86ba3daa75c3a44d6c14ff
zach <zachary.greathouse@hume.ai>
2024-05-09 16:17:12 -0700

    adds additional utility functions

--- src/wrapper/index.ts
+++ src/wrapper/index.ts
@@ -1,3 +1,8 @@
 export { base64Decode } from "./base64Decode";
 export { base64Encode } from "./base64Encode";
+export { convertBase64ToBlob } from "./convertBase64ToBlob";
+export { convertBlobToBase64 } from "./convertBlobToBase64";
+export { ensureSingleValidAudioTrack } from "./ensureSingleValidAudioTrack";
+export { getAudioStream } from "./getAudioStream";
+export { MimeType, getBrowserSupportedMimeType } from "./getBrowserSupportedMimeType";
 export { HumeClient } from "./HumeClient";
