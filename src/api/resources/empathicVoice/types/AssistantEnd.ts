16278c8d93c2883394de645eca663a1df0a0c3da
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-07-11 11:43:55 +0000

    SDK regeneration

--- src/api/resources/empathicVoice/types/AssistantEnd.ts
+++ src/api/resources/empathicVoice/types/AssistantEnd.ts
@@ -7,7 +7,7 @@
  */
 export interface AssistantEnd {
     /** The type of message sent through the socket; for an Assistant End message, this must be `assistant_end`. */
-    type: "assistant_message";
+    type: "assistant_end";
     /** Used to manage conversational state, correlate frontend and backend data, and persist conversations across EVI sessions. */
     customSessionId?: string;
 }
