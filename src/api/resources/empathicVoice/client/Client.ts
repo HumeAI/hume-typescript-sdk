fabf5e0b2fe131b57dd41553323ced8b442611e1
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-07-11 00:01:54 +0000

    SDK regeneration

--- src/api/resources/empathicVoice/client/Client.ts
+++ src/api/resources/empathicVoice/client/Client.ts
@@ -9,7 +9,6 @@ import { Prompts } from "../resources/prompts/client/Client";
 import { Configs } from "../resources/configs/client/Client";
 import { Chats } from "../resources/chats/client/Client";
 import { ChatGroups } from "../resources/chatGroups/client/Client";
-import { Chat } from "../resources/chat/client/Client";
 
 export declare namespace EmpathicVoice {
     interface Options {
@@ -59,11 +58,5 @@ export class EmpathicVoice {
 
     public get chatGroups(): ChatGroups {
         return (this._chatGroups ??= new ChatGroups(this._options));
-    }
-
-    protected _chat: Chat | undefined;
-
-    public get chat(): Chat {
-        return (this._chat ??= new Chat(this._options));
     }
 }
