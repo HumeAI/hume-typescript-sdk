742c1dbb49d564763ba7acd3df4940e3ffd736b2
Zach Krall <zach@zachkrall.com>
2024-07-17 11:24:04 -0400

    remove type assertion from fetch access token

--- src/wrapper/fetchAccessToken.ts
+++ src/wrapper/fetchAccessToken.ts
@@ -1,12 +1,12 @@
 import { base64Encode } from "./base64Encode";
+import { z } from "zod";
 
 /**
  * Fetches a new access token from the Hume API using the provided API key and Secret key.
  *
  * @param args - The arguments for the request.
- * @returns Promise that resolves to the new access token.
+ * @returns Promise that resolves to the new access token or null.
  * @throws If the base64 encoding fails.
- * @throws If the network request fails.
  * @example
  * ```typescript
  * async function getToken() {
@@ -14,34 +14,59 @@ import { base64Encode } from "./base64Encode";
  *     apiKey: 'test',
  *     secretKey: 'test',
  *   });
+ * 
  *   console.log(accessToken); // Outputs the access token
  * }
  * ```
  */
-export const fetchAccessToken = async (args: {
+export const fetchAccessToken = async ({
+    apiKey,
+    secretKey,
+    host = "api.hume.ai",
+}: {
     apiKey: string;
     secretKey: string;
     host?: string;
-  }): Promise<string> => {
-    const { apiKey, secretKey, host = 'api.hume.ai' } = args;
-  
+}): Promise<string | null> => {
     const authString = `${apiKey}:${secretKey}`;
     const encoded = base64Encode(authString);
-  
-    const res = await fetch(`https://${host}/oauth2-cc/token`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: `Basic ${encoded}`,
-      },
-      body: new URLSearchParams({
-        grant_type: 'client_credentials',
-      }).toString(),
-      cache: 'no-cache',
-    });
-  
-    const data = (await res.json()) as { access_token: string };
-    const accessToken = String(data['access_token']);
-  
-    return accessToken;
-  };
+
+    const response = await fetch(`https://${host}/oauth2-cc/token`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: `Basic ${encoded}`,
+        },
+        body: new URLSearchParams({
+            grant_type: "client_credentials",
+        }).toString(),
+        cache: "no-cache",
+    })
+        .then((res) => {
+            // if reading response as json fails, return empty object
+            // this can happen when request returns XML due to server error
+            return res
+                .json()
+                .then((d: unknown) => d)
+                .catch(() => ({}));
+        })
+        .then((data: unknown) => {
+            // extract access_token value from received object
+            return z
+                .object({
+                    access_token: z.string(),
+                })
+                .transform((data) => {
+                    return data.access_token;
+                })
+                .safeParse(data);
+        }).catch(() => ({
+          success: false
+        } as const));
+
+    if (!response.success) {
+        return null;
+    }
+
+    return response.data;
+};
