8b87358e8f871043472d33fa00ed10ad9a0cab0e
Deep Singhvi <deep@buildwithfern.com>
2024-07-28 14:15:35 -0400

    (feat): websocket respects environment (#94)

--- src/core/fetcher/Supplier.ts
+++ src/core/fetcher/Supplier.ts
@@ -1,7 +1,7 @@
-export type Supplier<T> = T | Promise<T> | (() => T | Promise<T>);
+export type Supplier<T> = T | (() => T);
 
 export const Supplier = {
-  get: async <T>(supplier: Supplier<T>): Promise<T> => {
+  get: <T>(supplier: Supplier<T>): T => {
     if (typeof supplier === 'function') {
       return (supplier as () => T)();
     } else {
