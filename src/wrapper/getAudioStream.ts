5533e4478f75c3023e393fa3370e349b8f8f931f
Chris Gregory <8800689+gregorybchris@users.noreply.github.com>
2025-04-17 11:13:58 -0700

    Add params to configure audio stream (#337)

--- src/wrapper/getAudioStream.ts
+++ src/wrapper/getAudioStream.ts
@@ -5,12 +5,16 @@
  * @returns {Promise<MediaStream>} A promise that resolves to a `MediaStream` containing audio data only.
  * @throws {DOMException} If the user denies access or no audio input devices are found.
  */
-export const getAudioStream = async (): Promise<MediaStream> => {
+export const getAudioStream = async (
+    echoCancellation: boolean = true,
+    noiseSuppression: boolean = true,
+    autoGainControl: boolean = true,
+): Promise<MediaStream> => {
     return navigator.mediaDevices.getUserMedia({
         audio: {
-            echoCancellation: true,
-            noiseSuppression: true,
-            autoGainControl: true,
+            echoCancellation,
+            noiseSuppression,
+            autoGainControl,
         },
         video: false,
     });
