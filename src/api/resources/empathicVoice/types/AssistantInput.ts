949266375a734c9c9e438dd6fcd2f70565e3a040
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-05-29 16:45:01 +0000

    SDK regeneration

--- src/api/resources/empathicVoice/types/AssistantInput.ts
+++ src/api/resources/empathicVoice/types/AssistantInput.ts
@@ -11,7 +11,7 @@ export interface AssistantInput {
     /**
      * Assistant text to synthesize into spoken audio and insert into the conversation.
      *
-     * EVI uses this text to generate spoken audio using our proprietary expressive text-to-speech model. Our model adds appropriate emotional inflections and tones to the text based on the user’s expressions and the context of the conversation. The synthesized audio is streamed back to the user as an [Assistant Message](/reference/empathic-voice-interface-evi/chat/chat#receive.Assistant%20Message.type).
+     * EVI uses this text to generate spoken audio using our proprietary expressive text-to-speech model. Our model adds appropriate emotional inflections and tones to the text based on the user’s expressions and the context of the conversation. The synthesized audio is streamed back to the user as an [Assistant Message](/reference/empathic-voice-interface-evi/chat/chat#receive.AssistantMessage.type).
      */
     text: string;
     /** The type of message sent through the socket; must be `assistant_input` for our server to correctly identify and process it as an Assistant Input message. */
