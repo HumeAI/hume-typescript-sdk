44312d91fad940a95355edc772fe421e24fa2f4e
twitchard <richard.marmorstein@gmail.com>
2025-06-17 11:18:32 -0700

    Format

--- src/wrapper/expressionMeasurement/batch/BatchClient.ts
+++ src/wrapper/expressionMeasurement/batch/BatchClient.ts
@@ -1,7 +1,7 @@
 import { Batch as FernClient } from "../../../api/resources/expressionMeasurement/resources/batch/client/Client";
 import * as Hume from "../../../api";
 import { Job } from "./Job";
-import * as core from "core"
+import * as core from "core";
 
 export class BatchClient extends FernClient {
     // This just wraps the return value of the base class's `startInferenceJob` method
@@ -10,8 +10,13 @@ export class BatchClient extends FernClient {
         request: Hume.expressionMeasurement.batch.InferenceBaseRequest = {},
         requestOptions?: FernClient.RequestOptions,
     ): core.HttpResponsePromise<Job> {
-        return core.HttpResponsePromise.fromPromise(super.startInferenceJob(request, requestOptions).withRawResponse().then((result) => {
-            return { data: new Job(result.data.jobId, this), rawResponse: result.rawResponse };
-        }))
+        return core.HttpResponsePromise.fromPromise(
+            super
+                .startInferenceJob(request, requestOptions)
+                .withRawResponse()
+                .then((result) => {
+                    return { data: new Job(result.data.jobId, this), rawResponse: result.rawResponse };
+                }),
+        );
     }
 }
