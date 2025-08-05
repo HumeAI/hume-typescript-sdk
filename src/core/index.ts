0a94829c1d9b369615cf28d22466098950f6c5a3
twitchard <richard.marmorstein@gmail.com>
2025-08-05 14:48:55 -0700

    Missing from core

--- src/core/index.ts
+++ src/core/index.ts
@@ -5,6 +5,7 @@ export * from "./stream";
 export * from "./utils";
 export * from "./pagination";
 export * from "./form-data-utils";
+export * from "./file";
 export * as serialization from "./schemas";
 
 // This one is manually added
