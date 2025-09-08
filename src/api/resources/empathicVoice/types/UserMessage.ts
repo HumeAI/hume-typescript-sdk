100e11cfced86fe1c3797e47a56d40e97a626cab
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-08 17:05:29 +0000

    SDK regeneration

--- src/api/resources/empathicVoice/types/UserMessage.ts
+++ src/api/resources/empathicVoice/types/UserMessage.ts
@@ -11,7 +11,7 @@ export interface UserMessage {
     /**
      * The type of message sent through the socket; for a User Message, this must be `user_message`.
      *
-     * This message contains both a transcript of the user's input and the expression measurement predictions if the input was sent as an [Audio Input message](/reference/empathic-voice-interface-evi/chat/chat#send.AudioInput.type). Expression measurement predictions are not provided for a [User Input message](/reference/empathic-voice-interface-evi/chat/chat#send.UserInput.type), as the prosody model relies on audio input and cannot process text alone.
+     * This message contains both a transcript of the user's input and the expression measurement predictions if the input was sent as an [Audio Input message](/reference/speech-to-speech-evi/chat#send.AudioInput). Expression measurement predictions are not provided for a [User Input message](/reference/speech-to-speech-evi/chat#send.UserInput), as the prosody model relies on audio input and cannot process text alone.
      */
     type: "user_message";
     /** Used to manage conversational state, correlate frontend and backend data, and persist conversations across EVI sessions. */
@@ -22,7 +22,7 @@ export interface UserMessage {
     models: Hume.empathicVoice.Inference;
     /** Start and End time of user message. */
     time: Hume.empathicVoice.MillisecondInterval;
-    /** Indicates if this message was inserted into the conversation as text from a [User Input](/reference/empathic-voice-interface-evi/chat/chat#send.UserInput.text) message. */
+    /** Indicates if this message was inserted into the conversation as text from a [User Input](/reference/speech-to-speech-evi/chat#send.UserInput.text) message. */
     fromText: boolean;
     /**
      * Indicates whether this `UserMessage` contains an interim (unfinalized) transcript.
@@ -30,7 +30,7 @@ export interface UserMessage {
      * - `true`: the transcript is provisional; words may be repeated or refined in subsequent `UserMessage` responses as additional audio is processed.
      * - `false`: the transcript is final and complete.
      *
-     * Interim transcripts are only sent when the [`verbose_transcription`](/reference/empathic-voice-interface-evi/chat/chat#request.query.verbose_transcription) query parameter is set to `true` in the initial handshake.
+     * Interim transcripts are only sent when the [`verbose_transcription`](/reference/speech-to-speech-evi/chat#request.query.verbose_transcription) query parameter is set to `true` in the initial handshake.
      */
     interim: boolean;
 }
