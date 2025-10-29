f7317a904649e004dc6ff76aef6c42aeda1a3c1b
twitchard <richard.marmorstein@gmail.com>
2025-10-29 10:42:28 -0500

    not fixing

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,4 @@ export { HumeError, HumeTimeoutError } from "./errors/index.js";
 export { HumeEnvironment, HumeEnvironmentUrls } from "./environments.js";
 export * from "./exports.js";
 
-export * from "./wrapper/index.js";
-export { ReconnectingWebSocket, CloseEvent as SocketCloseEvent } from "./core"
+export * from "./wrapper";
