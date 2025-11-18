5a6bae8a1dc151cb9b1a03a7f2a27413c30426e1
twitchard <richard.marmorstein@gmail.com>
2025-11-17 18:52:00 -0600

    access token needs love too

--- src/api/resources/empathicVoice/resources/chat/client/Client.ts
+++ src/api/resources/empathicVoice/resources/chat/client/Client.ts
@@ -147,7 +147,7 @@ export class Chat {
         // This `authHeaderValue` is manually added as if you don't provide it it will
         // be omitted from the headers which means it won't reach the logic in ws.ts that
         // extracts values from the headers and adds them to query parameters.
-        const authHeaderValue = core.Supplier.get(this._options.headers?.Authorization);
+        const authHeaderValue = core.Supplier.get(this._options.headers?.authorization);
         return { "X-Hume-Api-Key": apiKeyValue, Authorization: authHeaderValue };
     }
 }
