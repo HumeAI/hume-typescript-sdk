ad7045fdfeea6dfc2faefbcfecb7f46902c93569
ivaaan <diykarelia@gmail.com>
2025-11-24 20:55:00 +0100

    format

--- src/wrapper/expressionMeasurement/batch/BatchClient.ts
+++ src/wrapper/expressionMeasurement/batch/BatchClient.ts
@@ -3,39 +3,34 @@ import * as Hume from "../../../api/index.js";
 import { Job } from "./Job.js";
 import * as core from "../../../core/index.js";
 
-export class BatchClient extends FernClient
-{
+export class BatchClient extends FernClient {
     // This just wraps the return value of the base class's `startInferenceJob` and `startInferenceJobFromLocalFile` methods
     // and returns a `Job` instance (has helper functions to await the job's result) instead of a raw job ID.
-    public startInferenceJob (
+    public startInferenceJob(
         request: Hume.expressionMeasurement.batch.InferenceBaseRequest = {},
         requestOptions?: FernClient.RequestOptions,
-    ): core.HttpResponsePromise<Job>
-    {
+    ): core.HttpResponsePromise<Job> {
         return core.HttpResponsePromise.fromPromise(
             super
-                .startInferenceJob( request, requestOptions )
+                .startInferenceJob(request, requestOptions)
                 .withRawResponse()
-                .then( ( result ) =>
-                {
-                    return { data: new Job( result.data.jobId, this ), rawResponse: result.rawResponse };
-                } ),
+                .then((result) => {
+                    return { data: new Job(result.data.jobId, this), rawResponse: result.rawResponse };
+                }),
         );
     }
 
-    public startInferenceJobFromLocalFile (
+    public startInferenceJobFromLocalFile(
         request: Hume.expressionMeasurement.batch.BatchStartInferenceJobFromLocalFileRequest,
         requestOptions?: FernClient.RequestOptions,
-    ): core.HttpResponsePromise<Job>
-    {
+    ): core.HttpResponsePromise<Job> {
         return core.HttpResponsePromise.fromPromise(
             super
-                .startInferenceJobFromLocalFile( request, requestOptions )
+                .startInferenceJobFromLocalFile(request, requestOptions)
                 .withRawResponse()
-                .then( ( result ) =>
-                {
-                    return { data: new Job( result.data.jobId, this ), rawResponse: result.rawResponse };
-                } ),
-        );
+                .then((result) => {
+                    return { data: new Job(result.data.jobId, this), rawResponse: result.rawResponse };
+                }),
+v        );
     }
 }
