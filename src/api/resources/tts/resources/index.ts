c4d95469b70d9a540961da56aa234dfbb1c7d330
ivaaan <diykarelia@gmail.com>
2025-12-08 20:24:20 +0100

    apply Fern generated changes

--- src/api/resources/tts/resources/index.ts
+++ src/api/resources/tts/resources/index.ts
@@ -1,3 +1,3 @@
-export * as streamInput from "./streamInput/index.js";
-export * from "./voices/client/requests/index.js";
-export * as voices from "./voices/index.js";
+export * as streamInputClient from './streamInput/client/index.js';
+export * from './voices/client/requests/index.js';
+export * as voices from './voices/index.js';
---
+++ src/core/auth/AuthProvider.ts
@@ -0,0 +1,8 @@
+import type { EndpointMetadata } from '../fetcher/EndpointMetadata.js';
+import type { AuthRequest } from './AuthRequest.js';
+
+export interface AuthProvider {
+  getAuthRequest(arg?: {
+    endpointMetadata?: EndpointMetadata;
+  }): Promise<AuthRequest>;
+}
