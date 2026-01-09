3a565f36df4e6f0a68fb67c55d984a51bb491347
twitchard <richard.marmorstein@gmail.com>
2026-01-09 17:06:51 -0600

    closer to generated

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export * as Hume from "./api/index.js";
-export * as serialization from "./serialization/index.js";
+export type { BaseClientOptions, BaseRequestOptions } from "./BaseClient.js";
+export { HumeClient } from "./Client.js";
+export { HumeEnvironment, type HumeEnvironmentUrls } from "./environments.js";
 export { HumeError, HumeTimeoutError } from "./errors/index.js";
-export { HumeEnvironment } from "./environments.js";
-export type { HumeEnvironmentUrls } from "./environments.js";
 export * from "./exports.js";
-
+export * as serialization from "./serialization/index.js";
 export * from "./wrapper/index.js";
