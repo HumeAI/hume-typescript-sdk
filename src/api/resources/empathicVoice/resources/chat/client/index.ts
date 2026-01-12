a55724022622ea9826df89b0dd574e01d3b43d6e
ivaaan <diykarelia@gmail.com>
2026-01-12 20:01:22 +0100

    format

--- src/api/resources/empathicVoice/resources/chat/client/index.ts
+++ src/api/resources/empathicVoice/resources/chat/client/index.ts
@@ -1,3 +1,3 @@
 /** THIS FILE IS MANUALLY MAINTAINED: see .fernignore */
-export { ChatSocket } from './Socket.js';
-export { ChatClient as Chat } from './Client.js';
+export { ChatSocket } from "./Socket.js";
+export { ChatClient as Chat } from "./Client.js";
