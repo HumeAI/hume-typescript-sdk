9a0daf612d0ed25faa7dd2b79cd4fa8839dfbc4a
ivaaan <diykarelia@gmail.com>
2026-01-23 16:53:52 +0100

    break test

--- src/core/auth/AuthProvider.ts
+++ src/core/auth/AuthProvider.ts
@@ -1,6 +1,6 @@
-import type { EndpointMetadata } from "../fetcher/EndpointMetadata.js";
-import type { AuthRequest } from "./AuthRequest.js";
+import type { EndpointMetadata } from '../fetcher/EndpointMetadata.js';
+import type { AuthRequest } from './AuthRequest.js';
 
 export interface AuthProvider {
-    getAuthRequest(arg?: { endpointMetadata?: EndpointMetadata }): Promise<AuthRequest>;
+  // getAuthRequest(arg?: { endpointMetadata?: EndpointMetadata }): Promise<AuthRequest>;
 }
